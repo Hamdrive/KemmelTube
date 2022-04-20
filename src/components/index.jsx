551bb437e0e3906b3c377750ac04dd986acb0b51
Hamdrive <hamza.husein017@gmail.com>
2022-04-20 10:15:04 +0530

    [feat] setup basic files for navigation

---
+++ src/components/index.jsx
@@ -0,0 +1,4 @@
+import { Navbar } from "./navbar/Navbar";
+import { Sidebar } from "./Sidebar/Sidebar";
+
+export { Navbar, Sidebar };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="wrapper"></div>
+  );
 };

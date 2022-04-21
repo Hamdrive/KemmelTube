7859643f44bc049d4c27489020648945c74e5bd4
Hamdrive <hamza.husein017@gmail.com>
2022-04-21 23:53:36 +0530

    [feat] navbar for desktop

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,175 @@
-import React from 'react'
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import HomeIcon from "@mui/icons-material/Home";
+import ExploreIcon from "@mui/icons-material/Explore";
+import HistoryIcon from "@mui/icons-material/History";
+import ListIcon from "@mui/icons-material/List";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import WatchLaterIcon from "@mui/icons-material/WatchLater";
 
 export const Navbar = () => {
+  const [selected, setSelected] = useState("home");
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navigation">
+      <Box sx={{ width: "100%", maxWidth: 360 }}>
+        <List component="nav" aria-label="main mailbox folders">
+          <ListItemButton
+            sx={[
+              {
+                "&:hover": {
+                  bgcolor: "	#396ff9",
+                },
+              },
+              {
+                bgcolor: selected === "home" ? "#1a5bff" : "#1b1f27",
+                color: "#fff",
+                fontSize: 32,
+                borderRadius: "7px",
+                py: 1.75,
+              },
+            ]}
+            onClick={() => setSelected("home")}
+          >
+            <ListItemIcon>
+              <HomeIcon sx={{ fontSize: 26, color: "#fff" }} />
+            </ListItemIcon>
+            <ListItemText
+              primary="Home"
+              primaryTypographyProps={{ fontSize: 22, fontWeight: "medium" }}
+            />
+          </ListItemButton>
+          <ListItemButton
+            sx={[
+              {
+                "&:hover": {
+                  bgcolor: "	#396ff9",
+                },
+              },
+              {
+                bgcolor: selected === "explore" ? "#1a5bff" : "#1b1f27",
+                color: "#fff",
+                fontSize: 32,
+                borderRadius: "7px",
+                py: 1.75,
+              },
+            ]}
+            onClick={() => setSelected("explore")}
+          >
+            <ListItemIcon>
+              <ExploreIcon sx={{ fontSize: 26, color: "#fff" }} />
+            </ListItemIcon>
+            <ListItemText
+              primaryTypographyProps={{ fontSize: 22, fontWeight: "medium" }}
+              primary="Explore"
+            />
+          </ListItemButton>
+          <ListItemButton
+            sx={[
+              {
+                "&:hover": {
+                  bgcolor: "	#396ff9",
+                },
+              },
+              {
+                bgcolor: selected === "history" ? "#1a5bff" : "#1b1f27",
+                color: "#fff",
+                fontSize: 32,
+                borderRadius: "7px",
+                py: 1.75,
+              },
+            ]}
+            onClick={() => setSelected("history")}
+          >
+            <ListItemIcon>
+              <HistoryIcon sx={{ fontSize: 26, color: "#fff" }} />
+            </ListItemIcon>
+            <ListItemText
+              primaryTypographyProps={{ fontSize: 22, fontWeight: "medium" }}
+              primary="History"
+            />
+          </ListItemButton>
+          <ListItemButton
+            sx={[
+              {
+                "&:hover": {
+                  bgcolor: "	#396ff9",
+                },
+              },
+              {
+                bgcolor: selected === "playlists" ? "#1a5bff" : "#1b1f27",
+                color: "#fff",
+                fontSize: 32,
+                borderRadius: "7px",
+                py: 1.75,
+              },
+            ]}
+            onClick={() => setSelected("playlists")}
+          >
+            <ListItemIcon>
+              <ListIcon sx={{ fontSize: 26, color: "#fff" }} />
+            </ListItemIcon>
+            <ListItemText
+              primaryTypographyProps={{ fontSize: 22, fontWeight: "medium" }}
+              primary="Playlists"
+            />
+          </ListItemButton>
+          <ListItemButton
+            sx={[
+              {
+                "&:hover": {
+                  bgcolor: "	#396ff9",
+                },
+              },
+              {
+                bgcolor: selected === "liked" ? "#1a5bff" : "#1b1f27",
+                color: "#fff",
+                fontSize: 32,
+                borderRadius: "7px",
+                py: 1.75,
+              },
+            ]}
+            onClick={() => setSelected("liked")}
+          >
+            <ListItemIcon>
+              <ThumbUpIcon sx={{ fontSize: 26, color: "#fff" }} />
+            </ListItemIcon>
+            <ListItemText
+              primaryTypographyProps={{ fontSize: 22, fontWeight: "medium" }}
+              primary="Liked videos"
+            />
+          </ListItemButton>
+          <ListItemButton
+            sx={[
+              {
+                "&:hover": {
+                  bgcolor: "	#396ff9",
+                },
+              },
+              {
+                bgcolor: selected === "watchLater" ? "#1a5bff" : "#1b1f27",
+                color: "#fff",
+                fontSize: 32,
+                borderRadius: "7px",
+                py: 1.75,
+              },
+            ]}
+            onClick={() => setSelected("watchLater")}
+          >
+            <ListItemIcon>
+              <WatchLaterIcon sx={{ fontSize: 26, color: "#fff" }} />
+            </ListItemIcon>
+            <ListItemText
+              primaryTypographyProps={{ fontSize: 22, fontWeight: "medium" }}
+              primary="Watch later"
+            />
+          </ListItemButton>
+        </List>
+      </Box>
+    </nav>
+  );
+};
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,29 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import styles from "./Home.module.css"
+// import { Link } from "react-router-dom";
+import logo from "../../assets/logo1.png";
+import styles from "./Home.module.css";
+import { Navbar } from "../../components";
 
 export const Home = () => {
   return (
     <div className="wrapper">
       <section className={styles.page__layout}>
         <aside className={styles.sidebar}>
-          <nav className={styles.navigation}>
-            <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/explore">Explore</Link></li>
-              <li><Link to="/history">History</Link></li>
-              <li><Link to="/playlists">Playlist</Link></li>
-              <li><Link to="/liked">Liked videos</Link></li>
-              <li><Link to="/watchLater">Watch later</Link></li>
-            </ul>
-          </nav>
+          <div className={styles.aside__logo}>
+            <img src={logo} alt="logo" />
+          </div>
+          <Navbar />
         </aside>
         <section className={styles.main__section}>
           <header className={styles.header}>
             <div>
               <div className={styles.toggle}>
                 <div className={styles.hamburger}></div>
-                <div className={styles.logo}></div>
+                <div className={styles.header__logo}></div>
               </div>
               <div className={styles.settings}></div>
             </div>

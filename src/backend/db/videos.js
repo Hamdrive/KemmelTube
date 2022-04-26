f1d275e8db908155c814ce57568d9f2eaa3b9118
Hamdrive <hamza.husein017@gmail.com>
2022-04-26 22:15:02 +0530

    [feat] videos db

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -4,18 +4,150 @@
  * */
 
 export const videos = [
+  //V10s
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    _id: "FGINdQGaVh0",
+    title: "2005 Japanese Grand Prix: Race Highlights | DHL F1 Classics",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+      "Step back in time with us to Suzuka in 2005, when Kimi Raikkonen produced one of the greatest drives of his career",
+    creator: "F1",
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: "R22nxVYLaBQ",
+    title: "1998 Belgian Grand Prix: Race Highlights | DHL F1 Classics",
+    creator: "F1",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "You'd be forgiven for thinking this race peaked at the first corner crash, but how wrong you would be...",
+  },
+  {
+    _id: "hwcSaayPy7w",
+    title: "2005 Raikkonen's Suzuka Masterclass",
+    creator: "F1",
+    description:
+      "Was this the best drive of Kimi Raikkonen's career? The Iceman started 17th at Suzuka and ended up with the ninth win of his career - and what would be his final victory for McLaren.",
+  },
+  {
+    _id: "oe1iCcuoHl0",
+    title: "CHAOTIC RACE IN SEPANG - 2005 Malaysian Grand Prix",
+    creator: "F1",
+    description:
+      "An exhilirating, hot and chaotic race to finish line. Who comes out on top?",
+  },
+  {
+    _id: "pwHbwWiMGfw",
+    title: "One Of The Craziest Races: 1996 Monaco Grand Prix",
+    creator: "F1",
+    description:
+      "David Coulthard, Jacques Villeneuve and Jean Alesi look back on a classic grand prix",
+  },
+
+  //wtf1
+  {
+    _id: "OMN0oB0Waqw",
+    title: "What's more important: The Driver or the car?",
+    creator: "Chain Bear",
+    description:
+      "It's an age old question - is an F1 driver or an F1 car more important to success? Here we explore the question and come up with another crazy idea to figure this out once and for all.",
+  },
+  {
+    _id: "6OLVFa8YRfM",
+    title: "The Insane Logistics of Formula 1",
+    creator: "Wendover Productions",
+    description:
+      "No sport is as logistically challenging as motorsports. A chaotic ballet of trucks, boats, and planes transports this show across the world each year. Find out how it all comes together here.",
+  },
+  {
+    _id: "hgLQWIAaCmY",
+    title: "Formula 1 - Everything You Need to Know | Up to Speed",
+    creator: "Donut Media",
+    description:
+      "Formula One is the pinnacle of both driver ability and automotive design; The best drivers in the world driving the most advanced cars on the planet. Join James Pumphrey as he tells you everything you need to know about the world’s most  monumental motorsport.",
+  },
+  {
+    _id: "Zj8YB9ThPv4",
+    title: "How Much F1 Crashes Actually Cost",
+    creator: "Donut Media",
+    description:
+      "When a big accident happens, that sends teams looking through their couches for change, but also wondering who should pick up the bill. So today, we’re gonna look at the high cost of F1 wrecks, figure out why they’re so expensive.",
+  },
+  {
+    _id: "jGHqxKC-hAM",
+    title: "The Secret Evolution of F1 Cockpits",
+    creator: "Driver61",
+    description:
+      "F1 cockpits have changed over the years and have hidden many secret devices that have allowed Formula 1 drivers to make their cars faster. In this video, we take a look beyond the F1 steering wheel and delve into how Formula One drivers have managed their cars over the decades.",
+  },
+
+  //Insp Seb
+  {
+    _id: "MPs8odrk44M",
+    title: "Inspector Seb at Work",
+    creator: "ThatOneFormula",
+    description:
+      "Compilation of Inspector Seb at work at various events on the calendar",
+  },
+  {
+    _id: "4f7uO_QKwVk",
+    title: "Inspector Seb",
+    creator: "ThatOneFormula",
+    description:
+      "What's Inspector Seb upto this team? Who is he investingating? Find out here.",
+  },
+  {
+    _id: "RJqHzADFm3I",
+    title: "Inspector SEB on the mission",
+    creator: "ABC Sport",
+    description:
+      "Inspector Seb upto his usual antics, can he find what he yearns to know?",
+  },
+  {
+    _id: "LH9yXEpoMEw",
+    title: "Sebastian Vettel Names EVERY F1 Champion",
+    creator: "FORMULA 1",
+    description:
+      "World champion, inspector, comedian, environmentalist, and now, trivia expert. Here's how Sebastian Vettel absolutely dominated the competition on Grill the Grid",
+  },
+  {
+    _id: "BTrsVWHTQXQ",
+    title: "F1 2021 Qatar GP drivers press conference Sebastian Vettel",
+    creator: "Not only racing",
+    description:
+      "Inspector Seb interrogates the young and talented Lando Norris. Find out what he learns in this episode.",
+  },
+
+  //F1 memes
+  {
+    _id: "SX-sU7oosL8",
+    title: "Funniest F1 Moments Of 2021",
+    creator: "FORMULA 1",
+    description:
+      "'Can you hear me now?' 'No negative.' We picked out just a few of the funniest moments from 2021 - let us know your favourite! ",
+  },
+  {
+    _id: "PKqqQ1qitec",
+    title: "F1 Animated! 2019 Season",
+    creator: "FORMULA 1",
+    description:
+      "Take some out-of-context radio messages, some random commentary, and animator Nick Murray Willis... what do you get? F1 Animated, of course! Take a look at some re-imaginings of the 2019 season.",
+  },
+  {
+    _id: "NDrH1WbGqCE",
+    title:
+      "Alphamaxnova1 2020 SEASON RECAP - w/ LightLune, MattZaba03 (OFFICIAL)",
+    creator: "LightLune",
+    description:
+      "Let's rewind back to an amazing, nailbiter and thrilling 2020 season of racing!",
+  },
+  {
+    _id: "E_P50Z2E69g",
+    title: "Daniil Kvyat Hardbass Compilation Remastered",
+    creator: "Dacha44",
+    description: "*Hardbass Intensifies*",
+  },
+  {
+    _id: "6ywT-YZam0g",
+    title: "F1 2021 Abu Dhabi Meme Recap",
+    creator: "Gross Jean",
+    description: "NO MICHEAL NOOOO",
   },
 ];

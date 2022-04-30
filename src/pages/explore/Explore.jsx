9209bfc3aec2e7b5dd5f4c5057c11ce8fd8634d4
Hamdrive <hamza.husein017@gmail.com>
2022-04-30 14:20:14 +0530

    [feat] padding utils

--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -4,7 +4,7 @@ import { CategoryFilter } from "../../components";
 export const Explore = () => {
   const [selectedCategory, setSelectedCategory] = useState("All");
   return (
-    <main className="wrapper">
+    <main className="wrapper px-1">
       <CategoryFilter
         selectedCategory={selectedCategory}
         setSelectedCategory={setSelectedCategory}

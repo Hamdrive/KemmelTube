0186b2c52b521997976809da96e238861d240efa
Hamdrive <hamza.husein017@gmail.com>
2022-04-30 18:06:20 +0530

    [update] video database with images

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -9,15 +9,23 @@ export const videos = [
     _id: "FGINdQGaVh0",
     categoryName: "Glorious V10s",
     title: "2005 Japanese Grand Prix: Race Highlights | DHL F1 Classics",
+    thumbnail:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651319931/Products%20Images/KemmelTube/Video%20Thumbnails/FGINdQGaVh0.jpg",
     description:
       "Step back in time with us to Suzuka in 2005, when Kimi Raikkonen produced one of the greatest drives of his career",
     creator: "F1",
+    creatorLogo:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651320097/Products%20Images/KemmelTube/Video%20Creators/formula1.jpg",
   },
   {
     _id: "R22nxVYLaBQ",
     categoryName: "Glorious V10s",
     title: "1998 Belgian Grand Prix: Race Highlights | DHL F1 Classics",
+    thumbnail:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651319914/Products%20Images/KemmelTube/Video%20Thumbnails/R22nxVYLaBQ.jpg",
     creator: "F1",
+    creatorLogo:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651320097/Products%20Images/KemmelTube/Video%20Creators/formula1.jpg",
     description:
       "You'd be forgiven for thinking this race peaked at the first corner crash, but how wrong you would be...",
   },
@@ -25,7 +33,11 @@ export const videos = [
     _id: "hwcSaayPy7w",
     categoryName: "Glorious V10s",
     title: "2005 Raikkonen's Suzuka Masterclass",
+    thumbnail:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651319887/Products%20Images/KemmelTube/Video%20Thumbnails/hwcSaayPy7w.jpg",
     creator: "F1",
+    creatorLogo:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651320097/Products%20Images/KemmelTube/Video%20Creators/formula1.jpg",
     description:
       "Was this the best drive of Kimi Raikkonen's career? The Iceman started 17th at Suzuka and ended up with the ninth win of his career - and what would be his final victory for McLaren.",
   },
@@ -33,7 +45,11 @@ export const videos = [
     _id: "oe1iCcuoHl0",
     categoryName: "Glorious V10s",
     title: "CHAOTIC RACE IN SEPANG - 2005 Malaysian Grand Prix",
+    thumbnail:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651319852/Products%20Images/KemmelTube/Video%20Thumbnails/oe1iCcuoHl0.jpg",
     creator: "F1",
+    creatorLogo:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651320097/Products%20Images/KemmelTube/Video%20Creators/formula1.jpg",
     description:
       "An exhilirating, hot and chaotic race to finish line. Who comes out on top?",
   },
@@ -41,7 +57,11 @@ export const videos = [
     _id: "pwHbwWiMGfw",
     categoryName: "Glorious V10s",
     title: "One Of The Craziest Races: 1996 Monaco Grand Prix",
+    thumbnail:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651319822/Products%20Images/KemmelTube/Video%20Thumbnails/pwHbwWiMGfw.jpg",
     creator: "F1",
+    creatorLogo:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651320097/Products%20Images/KemmelTube/Video%20Creators/formula1.jpg",
     description:
       "David Coulthard, Jacques Villeneuve and Jean Alesi look back on a classic grand prix",
   },
@@ -49,41 +69,61 @@ export const videos = [
   //wtf1
   {
     _id: "OMN0oB0Waqw",
-    categoryName: "What the F1",
+    categoryName: "What The F1",
     title: "What's more important: The Driver or the car?",
+    thumbnail:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651320229/Products%20Images/KemmelTube/Video%20Thumbnails/OMN0oB0Waqw.jpg",
     creator: "Chain Bear",
+    creatorLogo:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651320434/Products%20Images/KemmelTube/Video%20Creators/unnamed_sne8bg.jpg",
     description:
       "It's an age old question - is an F1 driver or an F1 car more important to success? Here we explore the question and come up with another crazy idea to figure this out once and for all.",
   },
   {
     _id: "6OLVFa8YRfM",
-    categoryName: "What the F1",
+    categoryName: "What The F1",
     title: "The Insane Logistics of Formula 1",
+    thumbnail:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651320248/Products%20Images/KemmelTube/Video%20Thumbnails/6OLVFa8YRfM.jpg",
     creator: "Wendover Productions",
+    creatorLogo:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651320443/Products%20Images/KemmelTube/Video%20Creators/channels4_profile_qczhrg.jpg",
     description:
       "No sport is as logistically challenging as motorsports. A chaotic ballet of trucks, boats, and planes transports this show across the world each year. Find out how it all comes together here.",
   },
   {
     _id: "hgLQWIAaCmY",
-    categoryName: "What the F1",
+    categoryName: "What The F1",
     title: "Formula 1 - Everything You Need to Know | Up to Speed",
+    thumbnail:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651320267/Products%20Images/KemmelTube/Video%20Thumbnails/hgLQWIAaCmY.jpg",
     creator: "Donut Media",
+    creatorLogo:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651320453/Products%20Images/KemmelTube/Video%20Creators/unnamed_sswf5d.jpg",
     description:
       "Formula One is the pinnacle of both driver ability and automotive design; The best drivers in the world driving the most advanced cars on the planet. Join James Pumphrey as he tells you everything you need to know about the world’s most  monumental motorsport.",
   },
   {
     _id: "Zj8YB9ThPv4",
-    categoryName: "What the F1",
+    categoryName: "What The F1",
     title: "How Much F1 Crashes Actually Cost",
+    thumbnail:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651320288/Products%20Images/KemmelTube/Video%20Thumbnails/Zj8YB9ThPv4.jpg",
     creator: "Donut Media",
+    creatorLogo:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651320453/Products%20Images/KemmelTube/Video%20Creators/unnamed_sswf5d.jpg",
     description:
       "When a big accident happens, that sends teams looking through their couches for change, but also wondering who should pick up the bill. So today, we’re gonna look at the high cost of F1 wrecks, figure out why they’re so expensive.",
   },
   {
     _id: "jGHqxKC-hAM",
-    categoryName: "What the F1",
+    categoryName: "What The F1",
     title: "The Secret Evolution of F1 Cockpits",
+    thumbnail:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651320307/Products%20Images/KemmelTube/Video%20Thumbnails/jGHqxKC-hAM.jpg",
     creator: "Driver61",
+    creatorLogo:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651320463/Products%20Images/KemmelTube/Video%20Creators/unnamed_blvbfe.jpg",
     description:
       "F1 cockpits have changed over the years and have hidden many secret devices that have allowed Formula 1 drivers to make their cars faster. In this video, we take a look beyond the F1 steering wheel and delve into how Formula One drivers have managed their cars over the decades.",
   },
@@ -93,7 +133,11 @@ export const videos = [
     _id: "MPs8odrk44M",
     categoryName: "Inspector Seb",
     title: "Inspector Seb at Work",
+    thumbnail:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651321241/Products%20Images/KemmelTube/Video%20Thumbnails/MPs8odrk44M.jpg",
     creator: "ThatOneFormula",
+    creatorLogo:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651321311/Products%20Images/KemmelTube/Video%20Creators/channels4_profile_pzzmgz.jpg",
     description:
       "Compilation of Inspector Seb at work at various events on the calendar",
   },
@@ -101,7 +145,11 @@ export const videos = [
     _id: "4f7uO_QKwVk",
     categoryName: "Inspector Seb",
     title: "Inspector Seb",
+    thumbnail:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651321225/Products%20Images/KemmelTube/Video%20Thumbnails/4f7uO_QKwVk.jpg",
     creator: "ThatOneFormula",
+    creatorLogo:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651321311/Products%20Images/KemmelTube/Video%20Creators/channels4_profile_pzzmgz.jpg",
     description:
       "What's Inspector Seb upto this team? Who is he investingating? Find out here.",
   },
@@ -109,7 +157,11 @@ export const videos = [
     _id: "RJqHzADFm3I",
     categoryName: "Inspector Seb",
     title: "Inspector SEB on the mission",
+    thumbnail:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651321170/Products%20Images/KemmelTube/Video%20Thumbnails/RJqHzADFm3I.jpg",
     creator: "ABC Sport",
+    creatorLogo:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651321335/Products%20Images/KemmelTube/Video%20Creators/unnamed_zdgrvw.jpg",
     description:
       "Inspector Seb upto his usual antics, can he find what he yearns to know?",
   },
@@ -117,17 +169,26 @@ export const videos = [
     _id: "LH9yXEpoMEw",
     categoryName: "Inspector Seb",
     title: "Sebastian Vettel Names EVERY F1 Champion",
-    creator: "FORMULA 1",
+    thumbnail:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651321131/Products%20Images/KemmelTube/Video%20Thumbnails/LH9yXEpoMEw.jpg",
+    creator: "F1",
+    creatorLogo:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651320097/Products%20Images/KemmelTube/Video%20Creators/formula1.jpg",
+
     description:
       "World champion, inspector, comedian, environmentalist, and now, trivia expert. Here's how Sebastian Vettel absolutely dominated the competition on Grill the Grid",
   },
   {
-    _id: "BTrsVWHTQXQ",
+    _id: "dDZzoYFCn6A",
     categoryName: "Inspector Seb",
-    title: "F1 2021 Qatar GP drivers press conference Sebastian Vettel",
-    creator: "Not only racing",
+    title: "Sebastian Vettel being a Comedian for 5 minutes straight",
+    thumbnail:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651321117/Products%20Images/KemmelTube/Video%20Thumbnails/dDZzoYFCn6A.jpg",
+    creator: "Richard Olsen",
+    creatorLogo:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651321311/Products%20Images/KemmelTube/Video%20Creators/channels4_profile_pzzmgz.jpg",
     description:
-      "Inspector Seb interrogates the young and talented Lando Norris. Find out what he learns in this episode.",
+      "Join us for a sneak peek into the other lives of Inspector Seb, from driver to panelist to comedian",
   },
 
   //F1 memes
@@ -135,7 +196,11 @@ export const videos = [
     _id: "SX-sU7oosL8",
     categoryName: "F1 Memes",
     title: "Funniest F1 Moments Of 2021",
-    creator: "FORMULA 1",
+    thumbnail:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651321824/Products%20Images/KemmelTube/Video%20Thumbnails/SX-sU7oosL8.jpg",
+    creator: "F1",
+    creatorLogo:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651320097/Products%20Images/KemmelTube/Video%20Creators/formula1.jpg",
     description:
       "'Can you hear me now?' 'No negative.' We picked out just a few of the funniest moments from 2021 - let us know your favourite! ",
   },
@@ -143,16 +208,23 @@ export const videos = [
     _id: "PKqqQ1qitec",
     categoryName: "F1 Memes",
     title: "F1 Animated! 2019 Season",
-    creator: "FORMULA 1",
+    thumbnail:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651321803/Products%20Images/KemmelTube/Video%20Thumbnails/PKqqQ1qitec.jpg",
+    creator: "F1",
+    creatorLogo:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651320097/Products%20Images/KemmelTube/Video%20Creators/formula1.jpg",
     description:
       "Take some out-of-context radio messages, some random commentary, and animator Nick Murray Willis... what do you get? F1 Animated, of course! Take a look at some re-imaginings of the 2019 season.",
   },
   {
     _id: "NDrH1WbGqCE",
     categoryName: "F1 Memes",
-    title:
-      "Alphamaxnova1 2020 SEASON RECAP - w/ LightLune, MattZaba03 (OFFICIAL)",
-    creator: "LightLune",
+    title: "Alphamaxnova1 2020 SEASON RECAP",
+    thumbnail:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651321784/Products%20Images/KemmelTube/Video%20Thumbnails/NDrH1WbGqCE.jpg",
+    creator: "Alphamaxnova1",
+    creatorLogo:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651322123/Products%20Images/KemmelTube/Video%20Creators/unnamed_r7wcrt.jpg",
     description:
       "Let's rewind back to an amazing, nailbiter and thrilling 2020 season of racing!",
   },
@@ -160,14 +232,24 @@ export const videos = [
     _id: "E_P50Z2E69g",
     categoryName: "F1 Memes",
     title: "Daniil Kvyat Hardbass Compilation Remastered",
+    thumbnail:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651321766/Products%20Images/KemmelTube/Video%20Thumbnails/E_P50Z2E69g.jpg",
     creator: "Dacha44",
-    description: "*Hardbass Intensifies*",
+    creatorLogo:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651321934/Products%20Images/KemmelTube/Video%20Creators/unnamed_dhpwe9.jpg",
+    description:
+      "*Hardbass Intensifies* Follow Daniel Kyvat as he sends the missile on the racetrack in this compilation",
   },
   {
     _id: "6ywT-YZam0g",
     categoryName: "F1 Memes",
     title: "F1 2021 Abu Dhabi Meme Recap",
+    thumbnail:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651321742/Products%20Images/KemmelTube/Video%20Thumbnails/6ywT-YZam0g.jpg",
     creator: "Gross Jean",
-    description: "NO MICHEAL NOOOO",
+    creatorLogo:
+      "https://res.cloudinary.com/fueledup-ecom/image/upload/v1651321917/Products%20Images/KemmelTube/Video%20Creators/unnamed_dfm8fq.jpg",
+    description:
+      "NO MICHEAL NOOOO!! Watch the nail biting, agonizing and nerve wracking race of the century!",
   },
 ];

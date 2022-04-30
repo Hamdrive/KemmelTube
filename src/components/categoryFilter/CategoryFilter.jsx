0db4dc3acb5fa19e4bd27966a032daa29a3a4162
Hamdrive <hamza.husein017@gmail.com>
2022-04-30 14:19:58 +0530

    [update] hover/focus effect and reset btn

--- src/components/categoryFilter/CategoryFilter.jsx
+++ src/components/categoryFilter/CategoryFilter.jsx
@@ -1,23 +1,33 @@
 import React from "react";
 import { styled } from "@mui/material/styles";
-import Chip from "@mui/material/Chip";
-import Paper from "@mui/material/Paper";
 import { useVideo } from "../../context";
+import { Chip, Paper, Button } from "@mui/material";
 
 const ListItem = styled("li")(({ theme }) => ({
   margin: theme.spacing(0.5),
 }));
 
 const ChipItem = styled(Chip)(() => ({
+  "&&:hover": {
+    backgroundColor: "#396ff9",
+  },
+  "&&:focus": {
+    backgroundColor: "#1a5bff",
+  },
+
   padding: "8px",
   fontWeight: 400,
   fontSize: "14px",
+  color: "#fff",
+  border: "none",
+  backgroundColor: "#1b1f27",
 }));
 
 export const CategoryFilter = ({ selectedCategory, setSelectedCategory }) => {
   const {
     videoState: { categories },
   } = useVideo();
+  console.log(selectedCategory);
 
   return (
     <Paper
@@ -26,8 +36,9 @@ export const CategoryFilter = ({ selectedCategory, setSelectedCategory }) => {
         justifyContent: "flex-start",
         flexWrap: "wrap",
         listStyle: "none",
-        p: 0.5,
-        m: 0,
+        p: 1.5,
+        my: 3,
+        bgcolor: "#373c43",
       }}
       component="ul"
     >
@@ -36,6 +47,7 @@ export const CategoryFilter = ({ selectedCategory, setSelectedCategory }) => {
         <ChipItem
           label="All"
           variant={selectedCategory === "All" ? "filled" : "outlined"}
+          sx={{ backgroundColor: selectedCategory === "All" && "#1a5bff" }}
           onClick={() => setSelectedCategory("All")}
         />
       </ListItem>
@@ -51,6 +63,24 @@ export const CategoryFilter = ({ selectedCategory, setSelectedCategory }) => {
           />
         </ListItem>
       ))}
+
+      {/* Reset Category btn */}
+      <Button
+        sx={{
+          ml: 2,
+          p: 0.75,
+          backgroundColor: "#ed6c0299",
+          "&&:hover": {
+            backgroundColor: "#ed6c02d9",
+          },
+        }}
+        variant="contained"
+        size="small"
+        onClick={() => setSelectedCategory("All")}
+      >
+        Reset
+      </Button>
+
     </Paper>
   );
 };

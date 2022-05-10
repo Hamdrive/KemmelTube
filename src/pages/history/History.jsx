d0733063a3057cfb04a8e2e2ad7be821be40e23f
Hamdrive <hamza.husein017@gmail.com>
2022-05-10 08:33:30 +0530

    [feat] history page

--- src/pages/history/History.jsx
+++ src/pages/history/History.jsx
@@ -1,5 +1,30 @@
+import { Grid } from "@mui/material";
 import React from "react";
+import { ExploreCard } from "../../components";
+import { useVideo } from "../../context";
 
 export const History = () => {
-  return <div>History</div>;
+  const {
+    videoState: { history },
+  } = useVideo();
+
+  return (
+    <main className="wrapper p-1">
+      <Grid
+        columns={{ xs: 1, sm: 2, md: 3, lg: 5, xl: 4 }}
+        sx={{ gap: 3 }}
+        container>
+        {history.map((video) => (
+          <ExploreCard
+            key={video.id}
+            slug={video._id}
+            title={video.title}
+            thumbnail={video.thumbnail}
+            creator={video.creator}
+            creatorLogo={video.creatorLogo}
+          />
+        ))}
+      </Grid>
+    </main>
+  );
 };
